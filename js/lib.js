769234d3efd9053d7bd5598eebae56c800197d33
Markus Johansson <markus@parkus.se>
2018-12-31 00:56:28 +0100

    Showing pending XSS probes

--- js/lib.js
+++ js/lib.js
@@ -4,44 +4,78 @@
 
       // When set, alert has been called hence successful XSS
       window.XSS = true;
+      $('#success').show();
 
       // If in iframe, postmessage to parent to indicate successful xss - remove?
-      if (self!=top) {
-        window.parent.postMessage(location.href, "*");
+      let win = window.opener || top;
+      if (win) {
+        // For now assumed that vector is GET-based and calling window is window.opener or top
+        let xss_result = {vector: window.location.href, xss: true};
+        win.postMessage(xss_result, "*");
         return;
       } 
 
       // otherwise just show success banner 
       ret = proxied.apply(this, arguments);
-      $('#success').show();
       return ret;
   };
 })(window.alert);
 
 // Detect when automatic xss probe is successful
-function probe(url) {
-  return new Promise(function(resolve, reject) {
+function probe(url, timeout=null) {
+  var promises = [];
+  var iframe;
+
+  var p_xssvar = new Promise(function(resolve, reject) {
     function iframeLoadCallback(event) {
       let is_xss = this.contentWindow.XSS || false;
       let xss_result = {vector: url, xss: is_xss};
-      resolve(xss_result);
+      if (is_xss || timeout===null)
+        console.log(`XSS detected for url ${url}`);
+        resolve(xss_result);
     }
     iframe = document.createElement('iframe');
     iframe.src = url;
     iframe.style = "display: none;";
     iframe.addEventListener("load", iframeLoadCallback);
     document.body.appendChild(iframe);
-  })
-}
-
-// Probe mutliple payloads
-function test_payloads(payloads) {
-  return payloads.map(async function(url) {
-    console.log("Testing " + url);
-    result_prom = probe(url);
-    console.log(result_prom);
-    return result_prom;
   });
+  
+  // If timeout is defined, wait for max timeout ms and use postessage method in addition to window.XSS
+  if (timeout) {
+    let p_timeout = new Promise((resolve, reject) => {
+      let id = setTimeout(() => {
+        clearTimeout(id);
+        console.log(`Probe for ${url} timed out - no XSS detected`);
+        let xss_result = {xss: false, vector: url};
+        resolve(xss_result);
+      }, timeout)
+    });
+
+    let p_postmessage = new Promise ((resolve, reject) => {
+      function messageListener(event) {
+        if ("xss" in event.data && "vector" in event.data) {
+          if (event.source === iframe.contentWindow) {
+            let xss_result = {xss: true, vector: url};
+            resolve(xss_result);
+          }
+        }
+      }
+      window.addEventListener("message", messageListener, false);
+    });
+
+    promises.push(p_timeout);
+    promises.push(p_postmessage);
+  }
+
+  promises.push(p_xssvar);
+
+  // I'm not sure how this works - but it seems to do the job
+  function cleanUp() {
+    iframe.parentNode.removeChild(iframe);
+  }
+
+  return Promise.race(promises).finally(cleanUp);
 }
 
 function PerformXSSCheck() {
@@ -54,27 +88,54 @@ function PerformXSSCheck() {
     return text.split('\n');
   })
   .then(async function(payloads) {
-    let checks = test_payloads(payloads);
-    checks.forEach(async function(result) {
-      let xss_result = await result;
-      addXSSResult(xss_result);
+
+    // Add result to here
+    log = document.getElementById('check_log');
+
+    // Clean old result
+    while (log.firstChild) {
+        log.removeChild(log.firstChild);
+    }
+
+    // For each payload, create a pending probe and update when promise resolves
+    Array(...payloads.entries()).map(async function(item) {
+      let id = item[0];
+      let url = item[1];
+      console.log("Probing URL " + url);
+      pendingProbeElement(id, url);
+      result_prom = probe(url, 2500);
+      let xss_result = await result_prom;
+      updateProbeResult(id, xss_result);
+      return result_prom;
     });
   })
 
-async function addXSSResult(result) {
-  li = document.createElement('li');
+}
 
-  if (result.xss === true) {
-      li.className = "list-group-item list-group-item-danger glyphicon glyphicon-exclamation-sign";
-  }
-  else {
-      li.className = "list-group-item list-group-item-success glyphicon glyphicon-ok-circle";
-  }
+function pendingProbeElement(id, url) {
+  li = document.createElement('li');
+  li.id = "probe" + id;
+
+  li.className = "list-group-item list-group-item-warning glyphicon glyphicon-hourglass";
+
   link = document.createElement('a');
-  link.href = result.vector;
-  link.innerText = " " + result.vector;
-  li.appendChild(link)
+  link.href = url;
+  link.innerText = " " + url;
+
+  li.appendChild(link);
   log = document.getElementById('check_log');
   log.appendChild(li);
+  return li.id;
+}
+
+async function updateProbeResult(id, result) {
+  id = "probe" + id;
+  li = document.getElementById(id);
+
+  if (result.xss === true) {
+      li.className = "list-group-item list-group-item-danger glyphicon glyphicon-thumbs-down";
+  }
+  else {
+      li.className = "list-group-item list-group-item-success glyphicon glyphicon-thumbs-up";
   }
 }
